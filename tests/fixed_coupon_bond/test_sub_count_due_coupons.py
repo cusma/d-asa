6c8caae99a8c2568f5acfeea091bcb30bf7d49da
cusma <cosimo.bassi@algorand.foundation>
2025-02-05 18:11:26 -0300

    chore: lint

--- tests/fixed_coupon_bond/test_sub_count_due_coupons.py
+++ tests/fixed_coupon_bond/test_sub_count_due_coupons.py
@@ -1,10 +1,10 @@
-from algokit_utils import SigningAccount
+from algokit_utils import CommonAppCallParams, SendParams, SigningAccount
 
 from smart_contracts.artifacts.fixed_coupon_bond.fixed_coupon_bond_client import (
     FixedCouponBondClient,
 )
 from smart_contracts.fixed_coupon_bond import config as sc_cfg
-from tests.utils import DAsaConfig, time_warp
+from tests.utils import DAsaConfig, max_fee_per_coupon, time_warp
 
 
 def test_count_due_coupons_before_issuance(
@@ -25,13 +25,14 @@ def test_count_due_coupons_ongoing(
     for coupon in range(1, fixed_coupon_bond_cfg.total_coupons + 1):
         coupon_due_date = time_events[sc_cfg.FIRST_COUPON_DATE_IDX - 1 + coupon]
         time_warp(coupon_due_date)
-        # FIXME: cover_app_call_inner_transaction_fees seems not working with read-only methods
-        # due_coupons = fixed_coupon_bond_client_ongoing.send.get_coupons_status(
-        #     params=CommonAppCallParams(max_fee=max_fee_per_coupon(fixed_coupon_bond_cfg.total_coupons)),
-        #     send_params=SendParams(cover_app_call_inner_transaction_fees=True),
-        # ).abi_return.due_coupons
-        # print("Due coupons: ", due_coupons)
-        # assert due_coupons == coupon
+        due_coupons = fixed_coupon_bond_client_ongoing.send.get_coupons_status(
+            params=CommonAppCallParams(
+                max_fee=max_fee_per_coupon(fixed_coupon_bond_cfg.total_coupons)
+            ),
+            send_params=SendParams(cover_app_call_inner_transaction_fees=True),
+        ).abi_return.due_coupons
+        print("Due coupons: ", due_coupons)
+        assert due_coupons == coupon
 
 
 def test_count_due_coupons_at_maturity(
