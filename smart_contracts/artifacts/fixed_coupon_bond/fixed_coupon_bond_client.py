49dedc2b03e502a9792b5c671c03363de6568d5a
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2025-02-05 21:55:05 +0100

    refactor: updating to latest betas reflecting latest feedback

--- smart_contracts/artifacts/fixed_coupon_bond/fixed_coupon_bond_client.py
+++ smart_contracts/artifacts/fixed_coupon_bond/fixed_coupon_bond_client.py
@@ -59,7 +59,7 @@ def _init_dataclass(cls: type, data: dict) -> object:
         field_value = data.get(field.name)
         # Check if the field expects another dataclass and the value is a dict.
         if dataclasses.is_dataclass(field.type) and isinstance(field_value, dict):
-            field_values[field.name] = _init_dataclass(field.type, field_value)
+            field_values[field.name] = _init_dataclass(typing.cast(type, field.type), field_value)
         else:
             field_values[field.name] = field_value
     return cls(**field_values)
@@ -159,11 +159,19 @@ class AssetTransferArgs:
     receiver_holding_address: str
     units: int
 
+    @property
+    def abi_method_signature(self) -> str:
+        return "asset_transfer(address,address,uint64)uint64"
+
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class PayCouponArgs:
     """Dataclass for pay_coupon arguments"""
     holding_address: str
     payment_info: bytes | str
+
+    @property
+    def abi_method_signature(self) -> str:
+        return "pay_coupon(address,byte[])(uint64,uint64,byte[])"
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class PayPrincipalArgs:
@@ -171,16 +179,28 @@ class PayPrincipalArgs:
     holding_address: str
     payment_info: bytes | str
 
+    @property
+    def abi_method_signature(self) -> str:
+        return "pay_principal(address,byte[])(uint64,uint64,byte[])"
+
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class GetAccountUnitsCurrentValueArgs:
     """Dataclass for get_account_units_current_value arguments"""
     holding_address: str
     units: int
 
+    @property
+    def abi_method_signature(self) -> str:
+        return "get_account_units_current_value(address,uint64)(uint64,uint64,(uint64,uint64))"
+
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class GetPaymentAmountArgs:
     """Dataclass for get_payment_amount arguments"""
     holding_address: str
+
+    @property
+    def abi_method_signature(self) -> str:
+        return "get_payment_amount(address)(uint64,uint64)"
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class AssetConfigArgs:
@@ -196,10 +216,18 @@ class AssetConfigArgs:
     time_events: list[int]
     time_periods: list[tuple[int, int]]
 
+    @property
+    def abi_method_signature(self) -> str:
+        return "asset_config(uint64,uint64,uint64,uint64,uint64,uint8,uint16,uint16[],uint64[],(uint64,uint64)[])void"
+
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class SetSecondaryTimeEventsArgs:
     """Dataclass for set_secondary_time_events arguments"""
     secondary_market_time_events: list[int]
+
+    @property
+    def abi_method_signature(self) -> str:
+        return "set_secondary_time_events(uint64[])(uint64,uint64)"
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class AssignRoleArgs:
@@ -208,11 +236,19 @@ class AssignRoleArgs:
     role: int
     config: bytes | str
 
+    @property
+    def abi_method_signature(self) -> str:
+        return "assign_role(address,uint8,byte[])uint64"
+
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class RevokeRoleArgs:
     """Dataclass for revoke_role arguments"""
     role_address: str
     role: int
+
+    @property
+    def abi_method_signature(self) -> str:
+        return "revoke_role(address,uint8)uint64"
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class OpenAccountArgs:
@@ -220,10 +256,18 @@ class OpenAccountArgs:
     holding_address: str
     payment_address: str
 
+    @property
+    def abi_method_signature(self) -> str:
+        return "open_account(address,address)uint64"
+
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class CloseAccountArgs:
     """Dataclass for close_account arguments"""
     holding_address: str
+
+    @property
+    def abi_method_signature(self) -> str:
+        return "close_account(address)(uint64,uint64)"
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class PrimaryDistributionArgs:
@@ -231,10 +275,18 @@ class PrimaryDistributionArgs:
     holding_address: str
     units: int
 
+    @property
+    def abi_method_signature(self) -> str:
+        return "primary_distribution(address,uint64)uint64"
+
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class SetAssetSuspensionArgs:
     """Dataclass for set_asset_suspension arguments"""
     suspended: bool
+
+    @property
+    def abi_method_signature(self) -> str:
+        return "set_asset_suspension(bool)uint64"
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class SetAccountSuspensionArgs:
@@ -242,15 +294,27 @@ class SetAccountSuspensionArgs:
     holding_address: str
     suspended: bool
 
+    @property
+    def abi_method_signature(self) -> str:
+        return "set_account_suspension(address,bool)uint64"
+
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class SetDefaultStatusArgs:
     """Dataclass for set_default_status arguments"""
     defaulted: bool
 
+    @property
+    def abi_method_signature(self) -> str:
+        return "set_default_status(bool)void"
+
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class GetAccountInfoArgs:
     """Dataclass for get_account_info arguments"""
     holding_address: str
+
+    @property
+    def abi_method_signature(self) -> str:
+        return "get_account_info(address)(address,uint64,uint64,uint64,bool)"
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class AssetCreateArgs:
@@ -258,10 +322,18 @@ class AssetCreateArgs:
     arranger: str
     metadata: AssetMetadata
 
+    @property
+    def abi_method_signature(self) -> str:
+        return "asset_create(address,(uint8,uint8,uint8,uint8,uint8,uint8,byte[32],string))void"
+
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class AssetUpdateArgs:
     """Dataclass for asset_update arguments"""
     metadata: AssetMetadata
+
+    @property
+    def abi_method_signature(self) -> str:
+        return "asset_update((uint8,uint8,uint8,uint8,uint8,uint8,byte[32],string))void"
 
 
 class _FixedCouponBondUpdate:
@@ -2023,18 +2095,20 @@ class FixedCouponBondClient:
 @dataclasses.dataclass(frozen=True)
 class FixedCouponBondMethodCallCreateParams(
     algokit_utils.AppClientCreateSchema, algokit_utils.BaseAppClientMethodCallParams[
-        tuple[str, AssetMetadata] | AssetCreateArgs,
-        typing.Literal["asset_create(address,(uint8,uint8,uint8,uint8,uint8,uint8,byte[32],string))void"],
+        AssetCreateArgs,
+        str | None,
     ]
 ):
     """Parameters for creating FixedCouponBond contract using ABI"""
     on_complete: typing.Literal[OnComplete.NoOpOC] | None = None
+    method: str | None = None
 
     def to_algokit_utils_params(self) -> algokit_utils.AppClientMethodCallCreateParams:
         method_args = _parse_abi_args(self.args)
         return algokit_utils.AppClientMethodCallCreateParams(
             **{
                 **self.__dict__,
+                "method": self.method or getattr(self.args, "abi_method_signature", None),
                 "args": method_args,
             }
         )
@@ -2042,18 +2116,20 @@ class FixedCouponBondMethodCallCreateParams(
 @dataclasses.dataclass(frozen=True)
 class FixedCouponBondMethodCallUpdateParams(
     algokit_utils.BaseAppClientMethodCallParams[
-        tuple[AssetMetadata] | AssetUpdateArgs,
-        typing.Literal["asset_update((uint8,uint8,uint8,uint8,uint8,uint8,byte[32],string))void"],
+        AssetUpdateArgs,
+        str | None,
     ]
 ):
     """Parameters for calling FixedCouponBond contract using ABI"""
     on_complete: typing.Literal[OnComplete.UpdateApplicationOC] | None = None
+    method: str | None = None
 
     def to_algokit_utils_params(self) -> algokit_utils.AppClientMethodCallParams:
         method_args = _parse_abi_args(self.args)
         return algokit_utils.AppClientMethodCallParams(
             **{
                 **self.__dict__,
+                "method": self.method or getattr(self.args, "abi_method_signature", None),
                 "args": method_args,
             }
         )
@@ -2691,7 +2767,7 @@ class FixedCouponBondFactoryUpdateParams:
         """Updates an instance using a bare call"""
         params = params or algokit_utils.CommonAppCallCreateParams()
         return self.app_factory.params.bare.deploy_update(
-            algokit_utils.AppFactoryCreateParams(**dataclasses.asdict(params)),
+            algokit_utils.AppClientBareCallParams(**dataclasses.asdict(params)),
             )
 
 class FixedCouponBondFactoryDeleteParams:
@@ -2709,7 +2785,7 @@ class FixedCouponBondFactoryDeleteParams:
         """Deletes an instance using a bare call"""
         params = params or algokit_utils.CommonAppCallCreateParams()
         return self.app_factory.params.bare.deploy_delete(
-            algokit_utils.AppFactoryCreateParams(**dataclasses.asdict(params)),
+            algokit_utils.AppClientBareCallParams(**dataclasses.asdict(params)),
             )
 
 
--- tests/fixed_coupon_bond/test_sub_next_coupon_due_date.py
+++ tests/fixed_coupon_bond/test_sub_next_coupon_due_date.py
@@ -1,10 +1,15 @@
-from algokit_utils import AlgorandClient, SigningAccount
+from algokit_utils import (
+    AlgorandClient,
+    CommonAppCallParams,
+    SendParams,
+    SigningAccount,
+)
 
 from smart_contracts.artifacts.fixed_coupon_bond.fixed_coupon_bond_client import (
     FixedCouponBondClient,
 )
 from smart_contracts.fixed_coupon_bond import config as sc_cfg
-from tests.utils import DAsaConfig, time_warp
+from tests.utils import DAsaConfig, max_fee_per_coupon, time_warp
 
 TIME_LEFT_TO_DUE_DATE = 100  # Seconds
 
@@ -29,13 +34,14 @@ def test_next_coupon_due_date_ongoing(
     for coupon in range(1, fixed_coupon_bond_cfg.total_coupons + 1):
         coupon_due_date = time_events[sc_cfg.FIRST_COUPON_DATE_IDX - 1 + coupon]
         time_warp(coupon_due_date - TIME_LEFT_TO_DUE_DATE)
-        # FIXME: cover_app_call_inner_transaction_fees seems not working with read-only methods
-        # next_coupon_due_date = fixed_coupon_bond_client_ongoing.send.get_coupons_status(
-        #     params=CommonAppCallParams(max_fee=max_fee_per_coupon(fixed_coupon_bond_cfg.total_coupons)),
-        #     send_params=SendParams(cover_app_call_inner_transaction_fees=True),
-        # ).abi_return.next_coupon_due_date
-        # print("Next coupon due date: ", next_coupon_due_date)
-        # assert next_coupon_due_date == coupon_due_date
+        next_coupon_due_date = fixed_coupon_bond_client_ongoing.send.get_coupons_status(
+            params=CommonAppCallParams(
+                max_fee=max_fee_per_coupon(fixed_coupon_bond_cfg.total_coupons)
+            ),
+            send_params=SendParams(cover_app_call_inner_transaction_fees=True),
+        ).abi_return.next_coupon_due_date
+        print("Next coupon due date: ", next_coupon_due_date)
+        assert next_coupon_due_date == coupon_due_date
 
 
 def test_next_coupon_due_date_at_maturity(
