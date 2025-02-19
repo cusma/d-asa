9563ac3441e9d3de83eaf836637f43284b991fa1
cusma <cosimo.bassi@algorand.foundation>
2025-02-19 10:18:03 -0300

    chore: lint

--- tests/base_d_asa/conftest.py
+++ tests/base_d_asa/conftest.py
@@ -291,7 +291,7 @@ def account_with_units_factory(
 
 @pytest.fixture(scope="function")
 def account_a(
-    account_with_units_factory: Callable[..., utils.DAsaAccount]
+    account_with_units_factory: Callable[..., utils.DAsaAccount],
 ) -> utils.DAsaAccount:
     return account_with_units_factory()
 
