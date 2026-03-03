253dc73c08964a0d50147f813948e6860ecdde08
cusma <cosimo.bassi@algorand.foundation>
2026-03-03 11:00:46 +0100

    fix: args and return type

--- tests/module_rbac/test_rbac_get_address_role.py
+++ tests/module_rbac/test_rbac_get_address_role.py
@@ -2,6 +2,7 @@ from algokit_utils import SigningAccount
 
 from smart_contracts.artifacts.mock_module_rbac.mock_rbac_module_client import (
     MockRbacModuleClient,
+    RbacGetAddressRolesArgs,
 )
 from tests.utils import (
     DAsaAccountManager,
@@ -21,35 +22,39 @@ def test_pass_rbac_get_address_roles_for_assigned_accounts(
     interest_oracle: DAsaInterestOracle,
 ) -> None:
     account_manager_roles = rbac_client.send.rbac_get_address_roles(
-        account_manager.address
+        RbacGetAddressRolesArgs(address=account_manager.address)
     ).abi_return
-    assert account_manager_roles == (True, False, False, False, False)
+    assert account_manager_roles == [True, False, False, False, False]
 
     primary_dealer_roles = rbac_client.send.rbac_get_address_roles(
-        primary_dealer.address
+        RbacGetAddressRolesArgs(address=primary_dealer.address)
     ).abi_return
-    assert primary_dealer_roles == (False, True, False, False, False)
+    assert primary_dealer_roles == [False, True, False, False, False]
 
-    trustee_roles = rbac_client.send.rbac_get_address_roles(trustee.address).abi_return
-    assert trustee_roles == (False, False, True, False, False)
+    trustee_roles = rbac_client.send.rbac_get_address_roles(
+        RbacGetAddressRolesArgs(address=trustee.address)
+    ).abi_return
+    assert trustee_roles == [False, False, True, False, False]
 
     authority_roles = rbac_client.send.rbac_get_address_roles(
-        authority.address
+        RbacGetAddressRolesArgs(address=authority.address)
     ).abi_return
-    assert authority_roles == (False, False, False, True, False)
+    assert authority_roles == [False, False, False, True, False]
 
     interest_oracle_roles = rbac_client.send.rbac_get_address_roles(
-        interest_oracle.address
+        RbacGetAddressRolesArgs(address=interest_oracle.address)
     ).abi_return
-    assert interest_oracle_roles == (False, False, False, False, True)
+    assert interest_oracle_roles == [False, False, False, False, True]
 
 
 def test_pass_rbac_get_address_roles_for_unassigned_account(
     no_role_account: SigningAccount,
     rbac_client: MockRbacModuleClient,
 ) -> None:
-    roles = rbac_client.send.rbac_get_address_roles(no_role_account.address).abi_return
-    assert roles == (False, False, False, False, False)
+    roles = rbac_client.send.rbac_get_address_roles(
+        RbacGetAddressRolesArgs(address=no_role_account.address)
+    ).abi_return
+    assert roles == [False, False, False, False, False]
 
 
 def test_concrete_pass_rbac_get_address_roles(
@@ -58,11 +63,11 @@ def test_concrete_pass_rbac_get_address_roles(
     shared_client_empty,
 ) -> None:
     manager_roles = shared_client_empty.send.rbac_get_address_roles(
-        (shared_account_manager.address,)
+        RbacGetAddressRolesArgs(address=shared_account_manager.address)
     ).abi_return
-    assert manager_roles == (True, False, False, False, False)
+    assert manager_roles == [True, False, False, False, False]
 
     unassigned_roles = shared_client_empty.send.rbac_get_address_roles(
-        (no_role_account.address,)
+        RbacGetAddressRolesArgs(address=no_role_account.address)
     ).abi_return
-    assert unassigned_roles == (False, False, False, False, False)
+    assert unassigned_roles == [False, False, False, False, False]
