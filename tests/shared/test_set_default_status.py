d58fe42e9c834cfdec565a2c5107f6109baebf64
cusma <cosimo.bassi@algorand.foundation>
2026-03-03 17:48:56 +0100

    chore: move tests in core financial

--- tests/shared/test_set_default_status.py
+++ tests/shared/test_set_default_status.py
@@ -2,34 +2,6 @@ import pytest
 from algokit_utils import CommonAppCallParams, LogicError, SigningAccount
 
 from smart_contracts import errors as err
-from smart_contracts.artifacts.mock_module_rbac.mock_rbac_module_client import (
-    MockRbacModuleClient,
-    SetDefaultStatusArgs,
-)
-from tests.utils import DAsaTrustee
-
-
-def test_pass_set_default_status(
-    trustee: DAsaTrustee,
-    rbac_client: MockRbacModuleClient,
-) -> None:
-    assert not rbac_client.state.global_state.asset_defaulted
-    rbac_client.send.set_default_status(
-        SetDefaultStatusArgs(defaulted=True),
-        params=CommonAppCallParams(sender=trustee.address),
-    )
-    assert rbac_client.state.global_state.asset_defaulted
-
-
-def test_fail_unauthorized(
-    no_role_account: SigningAccount,
-    rbac_client: MockRbacModuleClient,
-) -> None:
-    with pytest.raises(LogicError, match=err.UNAUTHORIZED):
-        rbac_client.send.set_default_status(
-            SetDefaultStatusArgs(defaulted=True),
-            params=CommonAppCallParams(sender=no_role_account.address),
-        )
 
 
 def test_concrete_pass_set_default_status(
