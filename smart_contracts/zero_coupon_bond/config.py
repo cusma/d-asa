9c731fb64fdf3d4b1e6a8a819874b2fb3bcef48f
cusma <cosimo.bassi@algorand.foundation>
2026-03-12 16:55:49 +0100

    chore: remove deprecated files

--- smart_contracts/zero_coupon_bond/config.py
+++
@@ -1,9 +0,0 @@
-from typing import Final
-
-from smart_contracts import config as base_cfg
-
-# State Schema
-GLOBAL_BYTES: Final[int] = base_cfg.GLOBAL_BYTES
-GLOBAL_UINTS: Final[int] = base_cfg.GLOBAL_UINTS
-LOCAL_BYTES: Final[int] = base_cfg.LOCAL_BYTES
-LOCAL_UINTS: Final[int] = base_cfg.LOCAL_UINTS
